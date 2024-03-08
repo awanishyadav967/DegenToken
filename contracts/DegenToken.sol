339f978b238a2cce06eaf156e17509aa4a762ed7
Avanish Yadav <91160609+awanishyadav967@users.noreply.github.com>
2024-03-09 03:05:23 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -5,10 +5,50 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
+    struct Reward {
+        string name;
+        uint256 cost;
+    }
+    Reward[] public rewards; // List of available rewards
+    mapping(address => mapping(uint256 => bool)) public claimedRewards;
+    constructor() ERC20("Degen", "DGN") {
+        rewards.push(Reward("Official Degen NFT - ", 50));
+        rewards.push(Reward("Merchandise - ", 100));
+        rewards.push(Reward("Credits -", 100));
+    }
+    function getTotalRewards() public view returns (uint256) {
+        return rewards.length;
+    }
 
-    constructor() ERC20("Degen", "DGN") {}
+    function decimals() public pure override returns(uint8){
+        return 0;
+    }
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    function claimReward(uint256 rewardIndex) public {
+        require(rewardIndex < rewards.length, "Reward index out of bounds");
+        require(!claimedRewards[msg.sender][rewardIndex], "Reward already claimed");
+        require(balanceOf(msg.sender) >= rewards[rewardIndex].cost, "Insufficient tokens");
+        _approve(msg.sender,address(this),rewards[rewardIndex].cost);
+        transfer(address(this), rewards[rewardIndex].cost);
+        claimedRewards[msg.sender][rewardIndex] = true;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+    function transfer(address to,uint amount) public override returns(bool){
+        require(amount <= balanceOf(msg.sender), "ERC20: transfer amount exceeds balance");
+        _transfer(msg.sender, to, amount);
+        return false;
+    }
+
+    function shop() public view returns(Reward[] memory) {
+        return rewards;
+        
+    }
+
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
     }
 }
